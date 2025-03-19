d8621d9638ef622d3dc5be8fea3e35acf3a230d5
AskarAbdullaev <abdullaev25042000@gmail.com>
2025-03-19 16:38:30 +0100

    Update __init__.py

--- logex/__init__.py
+++ logex/__init__.py
@@ -1,4 +1,7 @@
 from . import propositional
 from . import formal_models
+from . import equational
 
-__all__ = ["propositional", "formal_models"]
+__all__ = ["propositional", "formal_models", "equational"]
+
+__version__ = '0.1.0'
